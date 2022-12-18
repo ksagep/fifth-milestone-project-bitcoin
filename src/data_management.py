8b1aa9c8aaca3654bccf77c15390ee9b2603a49d
ksagep <sandor.kisgyorgy@gmail.com>
2022-12-18 20:07:53 +0000

    Determine the database you want to use in data_management.py.

---
+++ src/data_management.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+
+# Determine the database you want to use
+@st.cache(supress_st_warning=True, allow_output_mutation=True)
+def_load bitcoin_data():
+    df = pd.read.csv("outputs/dataset/collection/Bitcoin_Price_Data.csv")
+    return df
