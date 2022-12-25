567e23e199d1796741d7418d015399b6080ee9ed
ksagep <sandor.kisgyorgy@gmail.com>
2022-12-25 19:59:54 +0000

    Finalize the conclusion part of the Study of annual comparison and complete the page project hypothesis and validation.

--- app_pages/page_project_hypothesis_and_validation.py
+++ app_pages/page_project_hypothesis_and_validation.py
@@ -21,8 +21,12 @@ def page_project_hypothesis_and_validation_body():
         f"as well as the events taking place in the world.\n\n"
 
         f"*Business requirements 2:*\n\n"
-        f"According to the results, as the exchange rate rises,\n"
-        f"the difference between the opening and closing value **will not be smaller** than with a lower exchange rate.\n"
-        f"When the exchange rate increases, the most of the time the opening price is higher than the closing price.\n"
-        f"During the sale, more attention should be paid to the opening price than to the closing price."
+        f"According to the plots, when the exchange rate increases,\n"
+        f"**most of the time the opening price is higher** than the closing price.\n"
+        f"According to the mathemathical method, as the exchange rate rises, the difference between the opening and closing value\n"
+        f"**will not be smaller** than with a lower exchange rate.\n"
+        f"The exchange rate was changing rapidly and the difference between the opening and closing prices were\n"
+        f"**significantly bigger from january 2018** than in the previous 4 years.\n"
+        f"During the sale, more attention should be paid to the opening price than to the closing price.\n"
+        f"When buying, it may be more beneficial to observe the closing price."
     )
