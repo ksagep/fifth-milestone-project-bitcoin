ba134132944df91f6b550b086a36ea6cec576da6
ksagep <sandor.kisgyorgy@gmail.com>
2022-12-28 22:18:22 +0000

    Delete the icon which personalized my app.

--- app_pages/multipage.py
+++ app_pages/multipage.py
@@ -6,11 +6,6 @@ class MultiPage:
     def __init__(self, app_name) -> None:
         self.pages = []
         self.app_name = app_name
-# Add an icon to personalize the app
-        st.set_page_config(
-            page_title=self.app_name,
-            page_icon="💵"
-        )
 
     def add_page(self, title, func) -> None:
         self.pages.append({"title": title, "function": func})
