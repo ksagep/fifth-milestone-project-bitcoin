a6eda0dd9393b76d5674664cd3d9f5e020678c0c
ksagep <sandor.kisgyorgy@gmail.com>
2023-01-28 13:42:10 +0000

    Develop the contain of page study of annual comparison.

--- app_pages/page_project_hypothesis_and_validation.py
+++ app_pages/page_project_hypothesis_and_validation.py
@@ -5,12 +5,12 @@ def page_project_hypothesis_and_validation_body():
 
     st.info(
         f"*Business requirements 1:*\n\n"
-        f"The client interested to understand the patterns from differences between opening and closing prices\n"
-        f"so the client can realize or decide the most relevant variables which are correlated as best option to sell Bitcoins.\n\n"
+        f"The client interested to understand the patterns from **differences between opening and closing prices**\n"
+        f"so the client can realize or decide the most relevant variables which are correlated as best option to sell or buy Bitcoins on a given day.\n\n"
 
         f"*Business requirements 2:*\n\n"
-        f"The client would like to know:\n\n"
-        f"As the exchange rate rises, the difference between the opening and closing value will be smaller than with a lower exchange rate."
+        f"The client would like to know that as the exchange rate rises, the difference between the opening and closing value will be smaller than with a lower exchange rate.\n"
+        f"How will correlate the opening and closing price to each other? Which period of the day could be the best for trading?"
     )
     # Conclusions taken from studies
     st.success(
--- app_pages/page_study_of_annual_comparison.py
+++ app_pages/page_study_of_annual_comparison.py
@@ -15,8 +15,6 @@ def page_study_of_annual_comparison_body():
     df = ("/workspace/fifth-milestone-project-bitcoin/app_pages/Bitcoin_Price_Data.csv")
     usecols=range(2,4)
 
-    newdf = df[df['Date'].str.contains('2014-03-14|01-01|06-30|12-31|2021-10-29')]
-    
     # Code copied from Study of annual comparison Notebook
     vars_to_study = ['Date', 'Closing price (USD)', '24h open (USD)']
 
@@ -38,30 +36,13 @@ def page_study_of_annual_comparison_body():
         f"When buying, it may be more beneficial to observe the closing price."
     )
 
-# Code based on EDA on selected variables of Study of annual comparison Notebook
+    if st.checkbox("Correlation between opening and closing price"):
 
-    def newdf_eda():
-        newdf_eda = newdf.filter(vars_to_study)
+        image = plt.imread('/workspace/fifth-milestone-project-bitcoin/assets/images/study_of_annual.png')
+        st.image(image)
 
-    if st.checkbox("Annual comparison between opening and closing price"):
-        annual_comparison(newdf_eda)
+    if st.checkbox("Difference between opening and closing price"):
 
-# Code copied from Study of annual comparison Notebook and adapted to page requirements
-def annual_comparison(df_eda):
-    df = ("/workspace/fifth-milestone-project-bitcoin/app_pages/Bitcoin_Price_Data.csv")
-    target_var = ['Date', 'Closing Price (USD)','24h Open (USD)']
-    vars_to_study = ['Date', 'Closing Price (USD)', '24h Open (USD)']
-    newdf = df[df['Date'].str.contains('2014-03-14|01-01|06-30|12-31|2021-10-29')]
-    newdf_eda = newdf.filter(vars_to_study)
-    for col in vars_to_study:
-    
-        plot_categorical(newdf_eda, col, target_var)
-        print()
-  
-# Code copied from Study of annual comparison Notebook and adapted to page requirements
-def plot_categorical(newdf, col, target_var):
-    df = ("/workspace/fifth-milestone-project-bitcoin/app_pages/Bitcoin_Price_Data.csv")
-    newdf = df[df['Date'].str.contains('2014-03-14|01-01|06-30|12-31|2021-10-29')]
-    plt.figure(figsize=(9, 5))
-    sns.regplot(x=newdf["24h Open (USD)"], y=newdf["Closing Price (USD)"])
-    plt.show()
+        image = plt.imread('/workspace/fifth-milestone-project-bitcoin/assets/images/study_of_annual_duo.png')
+        st.image(image)
+
