f1bada29d28c5c5c78124c1f92fb5e617e00685b
ksagep <sandor.kisgyorgy@gmail.com>
2023-01-29 20:37:47 +0000

    Create ML page on the dashboard.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from app_pages.page_summary import page_summary_body
 from app_pages.page_study_of_differences import page_study_of_differences_body
 from app_pages.page_study_of_annual_comparison import page_study_of_annual_comparison_body
 from app_pages.page_project_hypothesis_and_validation import page_project_hypothesis_and_validation_body
+from app_pages.page_ml_predict_exchange_rate import page_ml_predict_exchange_rate_body
 
 # Create an instance of the app
 app = MultiPage(app_name="Bitcoin - currency of matrix")
@@ -15,6 +16,7 @@ app.add_page("Short project summary", page_summary_body)
 app.add_page("Study of differences", page_study_of_differences_body)
 app.add_page("Study of annual comparison", page_study_of_annual_comparison_body)
 app.add_page("Project hypothesis and validation", page_project_hypothesis_and_validation_body)
+app.add_page("Exchange rate predictor - Bitcoin", page_ml_predict_exchange_rate_body)
 
 # Run the app
 app.run()
--- app_pages/page_project_hypothesis_and_validation.py
+++ app_pages/page_project_hypothesis_and_validation.py
@@ -4,20 +4,22 @@ def page_project_hypothesis_and_validation_body():
     st.write( "### Project hypothesis and validation")
 
     st.info(
-        f"1. *The opening price is lower than the closing price.*\n\n"
-	    f"   **Validation**: This will be validated with a correlation study and use diagrams for visualisation. Use mathematical calculation solution for better understanding of dataset.\n\n" 
-        f"2. *As the exchange rate rises, the difference between the opening and closing value will be smaller than with a lower exchange rate.*\n\n"
-        f"   **Validation**: This will be validated with a correlation study. Use diagrams for visualisation. Use mathematical calculation solution for better understanding of dataset.\n\n"
+        f"1. Hypothesis: *The opening price is lower than the closing price.*\n\n"
+	    f"**Validation**: This will be validated with a correlation study and use diagrams for visualisation.\n"
+        f"Use mathematical calculation solution for better understanding of dataset.\n\n" 
+        f"2. Hypothesis: *As the exchange rate rises, the difference between the opening and closing value will be smaller than with a lower exchange rate.*\n\n"
+        f"**Validation**: This will be validated with a correlation study.\n"
+        f"Use diagrams for visualisation. Use mathematical calculation solution for better understanding of dataset.\n\n"
     )
     # Conclusions taken from studies
     st.success(
-        f"*Business requirements 1:*\n\n"
+        f"*Hypothesis 1:*\n\n"
         f"The correlations and plots interpretation converge. The correlation was very strongly positive (0.99) because variables moved in the same direction.\n"
         f"The opening prices were higher more times than closing prices, **1450 vs 1337**.\n"
         f"When the client would like to sell Bitcoin, it is necessary to monitor the development of the exchange rate continuously,\n"
         f"as well as the events taking place in the world.\n\n"
 
-        f"*Business requirements 2:*\n\n"
+        f"*Hypothesis 2:*\n\n"
         f"According to the plots, when the exchange rate increases,\n"
         f"**most of the time the opening price is higher** than the closing price.\n"
         f"According to the mathemathical method, as the exchange rate rises, the difference between the opening and closing value\n"
